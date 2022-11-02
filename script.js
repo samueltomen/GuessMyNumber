3af37f7b0cdfe1aad22bf6b75c199fdd7eb0d225
Dytsaw <tomennana.samuel@outlook.fr>
2022-11-02 14:03:44 +0100

    conditions

--- script.js
+++ script.js
@@ -11,11 +11,16 @@ document.querySelector('.guess').value = 14;
 console.log(document.querySelector('.guess').value);
  */
 
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
     if(!guess){
         document.querySelector('.message').textContent= '🔞 No Number'
+    }else if(guess === secretNumber){
+        document.querySelector('.message').textContent = 'Bravo vous avez trouvez'
     }
     
 });
