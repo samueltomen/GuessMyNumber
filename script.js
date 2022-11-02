763bfe2f176073be5ed76fbf8dff932c5af615f7
Dytsaw <tomennana.samuel@outlook.fr>
2022-11-02 12:09:23 +0100

    handling click

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+/* console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number 🎉';
 
 document.querySelector('.number').textContent = 13;
@@ -9,3 +9,13 @@ document.querySelector('.score').textContent = 20;
 
 document.querySelector('.guess').value = 14;
 console.log(document.querySelector('.guess').value);
+ */
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+    if(!guess){
+        document.querySelector('.message').textContent= '🔞 No Number'
+    }
+    
+});
